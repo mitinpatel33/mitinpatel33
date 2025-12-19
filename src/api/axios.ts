4428f0897472e972fc34f0b2a050e77e001bd411
mitinpatel33 <mitinpatel99@gmail.com>
2025-12-19 15:45:01 +0530

    first commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.API_URL || 'http://localhost:5000/api'
+  baseURL: 'http://localhost:5000/api'
 });
 
 // Add token to headers if exists
